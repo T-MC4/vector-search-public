15fb79dd1bc87cae683cb23765a9dcdd82c242ad
t-mc4 <business.basics@aleeas.com>
2023-05-21 20:26:36 -0700

    Removed unnecessary comments

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -24,13 +24,17 @@ export default async function handler(
   const sanitizedQuestion = question.trim().replaceAll('\n', ' ');
 
   try {
-    // const index = pinecone.Index("short-filler-data")
     // const hrStart = process.hrtime();
     // console.time('Pinecone Timer');
-    const vectorStore = await embedDocs('', false);
-
     const t0 = performance.now();
 
+    const vectorStore = await embedDocs('', false);
+
+    // V1 Langchain code (remove comment and then comment V2)
+    // const result = await vectorStore.similaritySearch(sanitizedQuestion, 1);
+
+    // V2 Pinecone code
+    // const index = pinecone.Index("short-filler-data")
     const result = await vectorStore.pineconeIndex.query({
       queryRequest: {
         includeMetadata: true,
@@ -42,24 +46,12 @@ export default async function handler(
     const results = result.matches;
 
     const t1 = performance.now();
-    console.log(`This code took ${t1 - t0} milliseconds.`);
-
-    // const result = await vectorStore.similaritySearch(sanitizedQuestion, 1);
     // console.timeEnd('Pinecone Timer');
     // const hrEnd = process.hrtime(hrStart);
     // console.info('Execution time (hr): %ds %dms', hrEnd[0], hrEnd[1] / 1000000);
 
-    // Retrieve the Filler from local storage
-    // let text = '';
-    // let category = '';
-    // let fillerID = 0;
-    // for (let i of allShortFillers) {
-    //   if (i.metadata.fillerID === results[0][0].metadata.fillerID) {
-    //     text = i.pageContent;
-    //     category = i.metadata.category;
-    //     fillerID = i.metadata.fillerID;
-    //   }
-    // }
+    console.log(`This code took ${t1 - t0} milliseconds.`);
+
     const response = `${results[0].metadata.fillerText} (Category:${results[0].metadata.category} | ID:${results[0].metadata.fillerID} | TRAINING: ${results[0].pageContent})`;
     console.log(response);
 
@@ -68,12 +60,6 @@ export default async function handler(
       let prospectStatement = i.pageContent;
       let content = i.metadata.fillerText;
       let id = i.metadata.fillerText;
-      // for (let item of allShortFillers) {
-      //   if (item.metadata.fillerID === i[0].metadata.fillerID) {
-      //     content = item.pageContent;
-      //     id = item.metadata.fillerID;
-      //   }
-      // }
 
       history.push({
         content: prospectStatement,
